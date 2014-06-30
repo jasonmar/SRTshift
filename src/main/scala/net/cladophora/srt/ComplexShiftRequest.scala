4b2b697f4e637a0f0dec34b926bfdbfb1b872a79
Jason Mar <jasonmar@gmail.com>
2014-06-30 00:20:31 -0700

    Ignore .idea

--- src/main/scala/net/cladophora/srt/ComplexShiftRequest.scala
+++ src/main/scala/net/cladophora/srt/ComplexShiftRequest.scala
@@ -1,6 +1,3 @@
 package net.cladophora.srt
 
-/**
- * Created by Administrator on 6/29/2014.
- */
 case class ComplexShiftRequest (subs: Subtitles, t0: String, t0prime: String, t1: String, t1prime: String)
--- src/main/scala/net/cladophora/srt/SimpleShiftRequest.scala
+++ src/main/scala/net/cladophora/srt/SimpleShiftRequest.scala
@@ -1,6 +1,3 @@
 package net.cladophora.srt
 
-/**
- * Created by Administrator on 6/29/2014.
- */
 case class SimpleShiftRequest (subs: Subtitles, ms: Int)
